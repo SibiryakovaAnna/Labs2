4e8df2159b777d729cd300128822397944be188e
SibiryakovaAnna <46006009+SibiryakovaAnna@users.noreply.github.com>
2019-06-25 13:56:19 +0700

    Create code.h

---
+++ lab8/code.h
@@ -0,0 +1,23 @@
+#ifndef HUFFMAN_CODE_H
+#define HUFFMAN_CODE_H
+#include "Structs.h"
+#include "tree.h"
+
+Code* new_code(unsigned char* s, int size);
+void countFreq(int* freqTable, FILE* in);
+void base(Stack**stack, int* freqTable);
+void Resize(Code* buffer);
+unsigned char* copyCode(Code* buffer);
+void addBit(Code* buffer, int bit);
+void CodeTable(Code** codeTable, Tree* codeTree, Code* buffer);
+void writeBits(unsigned char bits, int count, BitStream* bitStream, FILE* out);
+void t_code(FILE* in, FILE* out, Code** codeTable, BitStream* bitStream);
+void writeSize(unsigned int size, FILE* out);
+void freeCodeTable(Code** codeTable);
+void code(FILE* in, FILE* out);
+unsigned char getBit(BitStream* bitStream, FILE* in);
+int readSize(FILE* in, unsigned int* size);
+void decodeText(Tree* codeTree, FILE* in, FILE* out, unsigned int size, BitStream* bitStream);
+void decode(FILE* in, FILE* out);
+
+#endif
