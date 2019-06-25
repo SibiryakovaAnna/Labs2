782d8b2ceecc3f9fc2fd211222e8c654cc120afe
SibiryakovaAnna <46006009+SibiryakovaAnna@users.noreply.github.com>
2019-06-25 14:01:19 +0700

    Create main.c

---
+++ lab8/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "code.h"
+ 
+int main() {
+    FILE* in = fopen("in.txt", "rb");
+    FILE* out = fopen("out.txt", "wb");
+    unsigned char mode = fgetc(in);
+    fseek(in, 3, SEEK_SET);
+    if (mode == 'c') {
+        code(in, out);
+    }
+    else if (mode == 'd') {
+        decode(in, out);
+    }
+    fclose(in);
+    fclose(out);
+    return 0;
+}
